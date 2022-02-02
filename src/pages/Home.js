f49f0e1fc4686dd746826a9a73493b5e822505ed
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-02 05:07:12 +0200

    finish the landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import Hero from "../components/Hero/Hero";
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+ 
+    }, 
   },
   plugins: [],
 }
