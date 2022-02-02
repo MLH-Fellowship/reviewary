3d5a49986c455201b58e72be2f0d8de2488fe99f
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-02 05:19:10 +0200

    add gradient

--- src/components/Hero/Login.js
+++ src/components/Hero/Login.js
@@ -4,16 +4,18 @@ import { ReactComponent as Mapillary } from "../../assets/mapilarry.svg";
 export default function Login() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center ml-0 lg:ml-20">
-        <div className="flex flex-row items-center justify-center gap-2 group">
-          <Mapillary className="h-16 w-16 transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-hover:scale-110" />
-          <h1 className="font-bold text-5xl capitalize text-gray-700">
-            Reviewary
-          </h1>
+      <div className="h-full flex flex-col items-center justify-center gap-7 ml-0 lg:ml-20">
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-row items-center justify-center gap-2 group">
+            <Mapillary className="h-16 w-16 transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-hover:scale-110" />
+            <h1 className="font-bold text-5xl capitalize text-gray-700">
+              Reviewary
+            </h1>
+          </div>
+          <p className="text-center  w-60 pt-3  text-gray-500">
+            explore the world from a different prospective
+          </p>
         </div>
-        <p className="text-center w-60 pt-3  text-gray-500">
-          explore the world from a different prospective
-        </p>
         <div className="flex flex-col gap-5 pt-10">
           <input
             type="text"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import Hero from "../components/Hero/Hero";
 export default function Home() {
   return (
     <>
-      <Hero />
+      <div className="h-screen bg-gradient-to-l  from-green-200 to-white ">
+        <Hero />
+      </div>
     </>
   );
 }
