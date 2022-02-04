8a04002415002cfbd7fcae16e47bcc0b5503d863
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-04 23:02:18 +0200

    solve prod warning

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,11 +1,9 @@
 import React from "react";
 import MapViewer from "../components/MapViewer/MapViewer";
-import Popup from "../components/Popup/Popup";
 
 export default function Map() {
   return (
     <>
-    
       <MapViewer />
     </>
   );
