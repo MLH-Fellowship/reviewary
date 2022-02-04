98ea3605e0c762e06107fdde83f88a7f0bede7f8
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-04 19:54:57 +0200

    fix screen height issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Hero from "../components/Hero/Hero";
 export default function Home() {
   return (
     <>
-      <div className="h-screen bg-gradient-to-t lg:bg-gradient-to-l  from-green-200 to-white ">
+      <div className="lg:h-screen bg-gradient-to-t lg:bg-gradient-to-l  from-green-200 to-white ">
         <Hero />
       </div>
     </>
