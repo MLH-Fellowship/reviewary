971599f9f78c4117b7d442a262852c99593956bd
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-02-04 23:24:19 +0200

    remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { useDispatch } from "react-redux";
 import { setLocationInfo } from "./redux/userSlice";
 import Home from "./pages/Home";
